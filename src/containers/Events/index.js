f8a675022006973f7a57dd2951a1f05ef5afc7e0
Ugo <pulito.ugo@gmail.com>
2023-04-17 22:48:49 +0200

    corr opé ternaire et slice pour pagination

--- src/containers/Events/index.js
+++ src/containers/Events/index.js
@@ -14,18 +14,8 @@ const EventList = () => {
   const [type, setType] = useState();
   const [currentPage, setCurrentPage] = useState(1);
   const filteredEvents = (
-    (!type
-      ? data?.events
-      : data?.events) || []
-  ).filter((event, index) => {
-    if (
-      (currentPage - 1) * PER_PAGE <= index &&
-      PER_PAGE * currentPage > index
-    ) {
-      return true;
-    }
-    return false;
-  });
+    (!type ? data?.events : data?.events.filter((event) => event.type === type)) || []
+  ).slice((currentPage - 1) * PER_PAGE, currentPage * PER_PAGE);
   const changeType = (evtType) => {
     setCurrentPage(1);
     setType(evtType);
