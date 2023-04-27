2127adeee383755e12b53d4c71d143665933b591
Ugo <pulito.ugo@gmail.com>
2023-04-27 19:51:54 +0200

    ajout waitfor pour validation test

--- src/containers/Form/index.test.js
+++ src/containers/Form/index.test.js
@@ -1,4 +1,4 @@
-import { fireEvent, render, screen } from "@testing-library/react";
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
 import Form from "./index";
 
 describe("When Events is created", () => {
@@ -22,8 +22,8 @@ describe("When Events is created", () => {
         })
       );
       await screen.findByText("En cours");
-      await screen.findByText("Envoyer");
-      expect(onSuccess).toHaveBeenCalled();
+      await waitFor(() => screen.queryByText("Envoyer"), { timeout: 6000 });
+      await waitFor(() =>expect(onSuccess).toHaveBeenCalled(), { timeout: 6000 });
     });
   });
 });
